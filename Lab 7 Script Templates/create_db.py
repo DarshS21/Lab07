e2be54dfb65779ce86bb69115600cd847a416843
DarshS21 <133030642+DarshS21@users.noreply.github.com>
2023-07-12 23:04:51 -0400

    Lab07
    Completed Script.

---
+++ Lab 7 Script Templates/create_db.py
@@ -0,0 +1,103 @@
+"""
+Description:
+ Creates the people table in the Social Network database
+ and populates it with 200 fake people.
+
+Usage:
+ python create_db.py
+"""
+import os
+import sqlite3
+from datetime import datetime
+from faker import Faker 
+
+# Determine the path of the database
+script_dir = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(script_dir, 'social_network.db')
+
+def main():
+    create_people_table()
+    populate_people_table()
+
+def create_people_table():
+    """Creates the people table in the database"""
+    # TODO: Create function body
+    # Hint: See example code in lab instructions entitled "Creating a Table"
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+
+    create_ppl_tbl_query = """
+        CREATE TABLE IF NOT EXISTS people
+        (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            address TEXT NOT NULL,
+            city TEXT NOT NULL,
+            province TEXT NOT NULL,
+            bio TEXT,
+            age INTEGER,
+            created_at DATETIME NOT NULL,
+            updated_at DATETIME NOT NULL
+        );
+    """
+    cur.execute(create_ppl_tbl_query)
+    con.commit()
+    con.close()
+
+
+def populate_people_table():
+    """Populates the people table with 200 fake people"""
+    # TODO: Create function body
+    # Hint: See example code in lab instructions entitled "Inserting Data into a Table"
+    # Hint: See example code in lab instructions entitled "Working with Faker"
+
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+
+    fake = Faker()
+
+    add_person_query = """
+        INSERT INTO people
+        (
+            name,
+            email,
+            address,
+            city,
+            province,
+            bio,
+            age,
+            created_at,
+            updated_at
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+    """
+    for _ in range(200):
+        name = fake.name()
+        email = fake.email()
+        address = fake.address()
+        city = fake.city()
+        province = fake.state()
+        bio = fake.text()
+        age = fake.random_int(min=1, max=100)
+        created_at = datetime.now()
+        updated_at = datetime.now()
+    
+        new_person = (
+            name,
+            email,
+            address,
+            city,
+            province,
+            bio,
+            age,
+            created_at,
+            updated_at
+        )
+    
+        cur.execute(add_person_query, new_person)
+        con.commit()
+    con.close()
+
+if __name__ == '__main__':
+   main()
